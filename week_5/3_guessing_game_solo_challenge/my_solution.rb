b6fbbc5b7f402d296e66dd06b19fe4855c49789f
lexieernst <ldernst@umich.edu>
2014-05-16 17:28:13 -0400

    worked on guessing game

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -14,11 +14,47 @@
 
 class GuessingGame
   def initialize(answer)
+  	@answer = answer.to_i
+  	@guess = guess.to_i
+  	#@guess = guess
+  	#g=gets.chomp
+  	#@guess = g.to_i
     # Your initialization code goes here
+    #g=gets.chomp
+  	#guess1 = g.to_i
   end
   
+  def guess
+
+  	puts "Guess a number"
+  	g=gets.chomp
+  	guess = g.to_i
+  	
+  	#@guess=@guess.gets.chomp
+
+  	if guess > @answer
+  		return ':high'
+  	elsif guess < @answer
+  		return ':low'
+  	else
+  		return ':correct'
+  	end
+  end
+
+  def solved
+  	#return false unless guess1 == @answer
+  	if guess != @answer
+  		return 'false'
+  	else
+  		return 'true'
+  	end
+  	
+  end
   # Make sure you define the other required methods, too
 end
+
+game = GuessingGame.new(10)
+puts game.solved
 
 
 
